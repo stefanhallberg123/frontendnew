cd46d9c479ccd7cf6d90e1fc310d2ff3c25d0369
[stefanhallberg123]”git config --global user.name [stefanhallberg123]” <[stefanhalllberg@live.se]>
2021-01-15 19:58:27 +0100

    fixing footer and navbar

--- src/components/customer/SendMsg/SendMsg.js
+++ src/components/customer/SendMsg/SendMsg.js
@@ -1,36 +1,18 @@
 import React, { useEffect } from "react";
-import Paper from "@material-ui/core/Paper";
-import Checkbox from "@material-ui/core/Checkbox";
-import Slide from "@material-ui/core/Slide";
-import { makeStyles } from "@material-ui/core/styles";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelopeOpen } from "@fortawesome/free-solid-svg-icons";
 import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 import Axios from "axios";
 import "./sendMsg.scss";
 
-// styles for msg-box
-const useStyles = makeStyles((theme) => ({
-  root: {
-    height: 200,
-  },
-  paper: {
-    zIndex: 10000000,
-    position: "fixed",
-    margin: theme.spacing(0),
-    width: "100%",
-    height: "100%",
-    // bottom: theme.spacing(10),
-    // right: theme.spacing(10),
-  },
-}));
-
 export default function SendMsg() {
   // show the form
   const [showModal, setShowModal] = React.useState(false);
+  // error validation
   const [error, setError] = React.useState("");
+  // confirmed validation
   const [sent, setSent] = React.useState("");
+  // when true scroll is locked
   useEffect(() => {
     if (showModal) {
       document.body.style.overflow = "hidden";
@@ -51,8 +33,7 @@ export default function SendMsg() {
     name: "",
     dataConsent: false,
   });
-  // adding the settings for material-ui
-  const classes = useStyles();
+
   // open and close the form
   const openModal = () => {
     setShowModal((c) => !c);
@@ -75,13 +56,10 @@ export default function SendMsg() {
       values.name === "" &&
       values.description === ""
     ) {
-      //   e.preventDefault();
       setError("Alla fält förutom registreringsnummer måste vara ifyllda");
     } else if (values.dataConsent === false) {
-      //   e.preventDefault();
       setError("Du måste godkänna att vi använder dina uppgifter");
     } else {
-      //   e.preventDefault();
       Axios.post(`http://localhost:5000/`, values)
         .then((respone) => {
           console.log(respone.data);
@@ -95,122 +73,114 @@ export default function SendMsg() {
       }, 2000);
     }
   };
-  // Axios.get("http://localhost/api/api/read.php").then((response) => {
-  //   console.log(response.data);
-  // });
-
   return (
-    <div className="message">
-      <div className="placer">
-        <FormControlLabel
-          control={<Checkbox checked={showModal} onChange={openModal} />}
-          label={
-            <FontAwesomeIcon className="emailIcon" icon={faEnvelopeOpen} />
-          }
-        />
+    <div className="placer">
+      <FontAwesomeIcon
+        className="emailIcon"
+        onClick={openModal}
+        icon={faEnvelopeOpen}
+      />
+      <div className={showModal ? "message" : "notMessage"}>
+        <form className="formish" onSubmit={submit}>
+          <FontAwesomeIcon
+            className="exitDSKTP"
+            onClick={openModal}
+            icon={faTimesCircle}
+          />
+          <h1 className="contacth1">Kontakta oss för frågor!</h1>
+          <div className="row">
+            <div className="col-25">
+              <label>Ämne:</label>
+            </div>
+            <div className="col-75">
+              <input
+                type="text"
+                placeholder="Ange ditt ämne"
+                value={values.subject}
+                name="subject"
+                onChange={changeData}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-25">
+              <label> Om frågan handlar om en bil</label>
+            </div>
+            <div className="col-75">
+              <input
+                placeholder="Registreringsnummer"
+                type="text"
+                value={values.regnumber || ""}
+                name="regnumber"
+                onChange={changeData}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-25">
+              <label>Email:</label>
+            </div>
+            <div className="col-75">
+              <input
+                placeholder="Ange ditt email"
+                type="email"
+                value={values.email}
+                name="email"
+                onChange={changeData}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-25">
+              <label>Namn:</label>
+            </div>
+            <div className="col-75">
+              <input
+                placeholder="Ange ditt namn"
+                type="text"
+                value={values.name}
+                name="name"
+                onChange={changeData}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-25">
+              <label>Beskrivning:</label>
+            </div>
+            <div className="col-75">
+              <textarea
+                className="textAnswer"
+                type="text"
+                placeholder="Beskriv din fråga här"
+                value={values.description}
+                name="description"
+                onChange={changeData}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <label className="gdprInfo">
+              Härmed godkänner du att P.ALBilhandel använder och sparar dina
+              ovanstående uppgifter för att svara på din fråga:
+              <input
+                name="dataConsent"
+                type="checkbox"
+                checked={values.dataConsent}
+                value={values.dataConsent}
+                onChange={changeData}
+              />
+            </label>
+          </div>
+          <div className="row">
+            <button className="msgBtn" type="submit">
+              Skicka
+            </button>
+            <div className="error">{error}</div>
+            <div className="sent">{sent}</div>
+          </div>
+        </form>
       </div>
-      <Slide direction="down" in={showModal} mountOnEnter unmountOnExit>
-        <Paper elevation={0} className={classes.paper}>
-          <form onSubmit={submit}>
-            <FontAwesomeIcon
-              className="exitDSKTP"
-              onClick={openModal}
-              icon={faTimesCircle}
-            />
-            <h1>Kontakta oss för frågor!</h1>
-            <div className="row">
-              <div className="col-25">
-                <label>Ämne:</label>
-              </div>
-              <div className="col-75">
-                <input
-                  type="text"
-                  placeholder="Ange ditt ämne"
-                  value={values.subject}
-                  name="subject"
-                  onChange={changeData}
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-25">
-                <label> Om frågan handlar om en bil</label>
-              </div>
-              <div className="col-75">
-                <input
-                  placeholder="Registreringsnummer"
-                  type="text"
-                  value={values.regnumber || ""}
-                  name="regnumber"
-                  onChange={changeData}
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-25">
-                <label>Email:</label>
-              </div>
-              <div className="col-75">
-                <input
-                  placeholder="Ange ditt email"
-                  type="email"
-                  value={values.email}
-                  name="email"
-                  onChange={changeData}
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-25">
-                <label>Namn:</label>
-              </div>
-              <div className="col-75">
-                <input
-                  placeholder="Ange ditt namn"
-                  type="text"
-                  value={values.name}
-                  name="name"
-                  onChange={changeData}
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-25">
-                <label>Beskrivning:</label>
-              </div>
-              <div className="col-75">
-                <textarea
-                  className="textAnswer"
-                  type="text"
-                  placeholder="Beskriv din fråga här"
-                  value={values.description}
-                  name="description"
-                  onChange={changeData}
-                />
-              </div>
-            </div>
-            <div className="row">
-              <label>
-                GDPR:
-                <input
-                  name="dataConsent"
-                  type="checkbox"
-                  checked={values.dataConsent}
-                  value={values.dataConsent}
-                  onChange={changeData}
-                />
-              </label>
-            </div>
-            <div className="row">
-              <button className="msgBtn" type="submit">
-                Svara
-              </button>
-              <div className="error">{error}</div>
-              <div className="sent">{sent}</div>
-            </div>
-          </form>
-        </Paper>
-      </Slide>
     </div>
   );
 }
--- src/components/customer/Topnav/Topnav.js
+++ src/components/customer/Topnav/Topnav.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+import { faInstagramSquare } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 import "./topNav.scss";
 
 export default function Topnav() {
+  const [toggle, setToggle] = useState(false);
+  useEffect(() => {
+    if (toggle) {
+      document.body.style.overflow = "hidden";
+      document.body.style.paddingRight = "15px";
+    }
+    return () => {
+      document.body.style.overflow = "unset";
+      document.body.style.paddingRight = "0px";
+    };
+  }, [toggle]);
+
+  const showNav = () => {
+    setToggle((c) => !c);
+  };
   return (
     <div>
       <nav className="navbarCustomer">
         <div className="burgerIcon">
-          <FontAwesomeIcon className="burger" icon={faBars} />
+          <FontAwesomeIcon
+            onClick={showNav}
+            className={toggle ? "turn" : "burger"}
+            icon={faBars}
+          />
         </div>
         <div className="NavLogoFlex">
           <Link to="/">
@@ -21,19 +43,27 @@ export default function Topnav() {
             />
           </Link>
         </div>
-        <div className="crap"></div>
+        <div className={toggle ? "navarea" : "notNavArea"}>
+          <h1 className="navHead">P.AlBilhandel</h1>
+          <Link to="/">Start</Link>
+          <Link to="/allCars">Alla våra bilar</Link>
+          <Link to="/about">Om oss</Link>
+          <Link to="/formedlaBil">Förmedla din bil</Link>
+          <h3>Kontakta oss på någon av länkarna nedan</h3>
+          <div className="SoMo">
+            <a href="https://www.instagram.com/p.albilhandel/">
+              <FontAwesomeIcon className="instagram" icon={faInstagramSquare} />
+            </a>
+            <a href="https://www.facebook.com/palbilhandel">
+              <FontAwesomeIcon className="facebook" icon={faFacebookSquare} />
+            </a>
+
+            <a href="mailto:info@palbilhandel.se">
+              <FontAwesomeIcon className="emailLogoFooter" icon={faEnvelope} />
+            </a>
+          </div>
+        </div>
       </nav>
     </div>
-
-    // <div>
-    //   {/* <FontAwesomeIcon className="burger" icon={faBars} />
-    //   <div className="container">
-    //     <img
-    //       className="logoImg"
-    //       src={`${process.env.PUBLIC_URL}/images/logoTransp.png`}
-    //       alt="logo"
-    //     />
-    //   </div> */}
-    // </div>
   );
 }
